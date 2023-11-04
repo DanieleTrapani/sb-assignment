b5f5dbe2545bc3ab3de08eb748f40ca2c0460668
Daniele Trapani <daniele.trapani96@gmail.com>
2023-11-04 16:52:07 +0100

    reworked header styling for routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,44 @@
 import SBLogo from "../assets/logo.png";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
-  const [selected, setSelected] = useState(0);
-  const changeSelected = (event) => {
-    if (event.target.classList.contains("selected")) {
-      return;
-    }
-    const selected = document.querySelector(".selected");
-    const unselected = document.querySelector(".unselected");
-    const pathTitle = document.querySelector(".path-title");
-    selected.classList.remove("selected");
-    selected.classList.add("unselected");
-    unselected.classList.remove("unselected");
-    unselected.classList.add("selected");
-    console.log(window.location.pathname);
-    const pathname = window.location.pathname;
-    if (pathname === "/blogs") {
-      pathTitle.classList.add("invisible");
-    } else {
-      pathTitle.classList.remove("invisible");
-    }
-    setSelected(selected);
-  };
+  const { pathname } = useLocation();
+  console.log(pathname);
 
   return (
     <header className="h-38 px-[10rem] py-8">
       <div className="flex justify-between items-center">
         <img src={SBLogo} alt="Social Brothers Logo" className="w-64" />
         <div className="flex gap-7">
-          <Link to="/" onClick={changeSelected}>
-            <h2 className="text-xl text-white py-1 selected">Home</h2>
+          <Link to="/">
+            <h2
+              className={
+                "text-xl text-white py-1" +
+                (pathname === "/" ? " selected" : "")
+              }
+            >
+              Home
+            </h2>
           </Link>
-          <Link to="/blogs" onClick={changeSelected}>
-            <h2 className="text-xl text-white py-1 unselected">Blog</h2>
+          <Link to="/blogs">
+            <h2
+              className={
+                "text-xl text-white py-1" +
+                (pathname === "/blogs" ? " selected" : "")
+              }
+            >
+              Blog
+            </h2>
           </Link>
         </div>
       </div>
-      <p className="text-white text-5xl text-center my-4 path-title invisible font-bold">
+      <p
+        className={
+          `text-white text-5xl text-center my-4 path-title font-bold` +
+          (pathname === "/" ? " invisible" : "")
+        }
+      >
         Blog
       </p>
     </header>
